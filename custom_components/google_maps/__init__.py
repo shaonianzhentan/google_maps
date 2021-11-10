80ebe0145ab0a79be6e52214c46419a7094933d3
zhulu <zhululu@weihaizixun.com>
2021-11-10 10:12:14 +0800

    初始化项目

---
+++ custom_components/google_maps/__init__.py
@@ -0,0 +1,22 @@
+import uuid
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+import homeassistant.helpers.config_validation as cv
+
+from .const import DOMAIN
+
+CONFIG_SCHEMA = cv.deprecated(DOMAIN)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    hass.http.register_static_path("/baidu_maps_www", hass.config.path("custom_components/" + DOMAIN + "/www"), False)
+    hass.components.frontend.async_register_built_in_panel(
+                        "iframe",
+                        "百度地图",
+                        "mdi:google-maps",
+                        "/baidu_maps",
+                        { "url": "/baidu_maps_www/index.html?v=" + uuid.uuid1().hex },
+                        require_admin=False)
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    return True
