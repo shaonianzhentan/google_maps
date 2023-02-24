db78440dedb92e555802a8f041c13734ceed931f
shaonianzhetnan <5294022+shaonianzhentan@users.noreply.github.com>
2023-02-24 23:00:18 +0800

    支持定位与缩放

--- custom_components/google_maps/www/map.js
+++ custom_components/google_maps/www/map.js
@@ -6,6 +6,14 @@ customElements.whenDefined("hui-view").then(() => {
 
         config = {}
         zoom = 16
+        loading = false
+
+        static getStubConfig() {
+            return { 
+                entity_id: "带经纬度属性的实体",
+                ak: '百度地图AK密钥'
+            }
+        }
 
         setConfig(config) {
             if (!config.entity_id) {
@@ -37,11 +45,28 @@ customElements.whenDefined("hui-view").then(() => {
             }
         }
 
+        fire(type, data) {
+            const event = new Event(type, {
+                bubbles: true,
+                cancelable: false,
+                composed: true
+            });
+            event.detail = data;
+            this.dispatchEvent(event);
+        }
+
+        showMoreInfo(entityId) {
+            this.fire('hass-more-info', { entityId })
+        }
+
         updated() {
             const { hass, config } = this
             let { entity_id, ak } = config
             if (hass && entity_id && ak) {
                 const entity = hass.states[entity_id]
+                if(!entity){                    
+                    throw new Error('你需要定义一个实体');
+                }
                 let attributes = entity['attributes']
                 if (Reflect.has(attributes, 'latitude') && Reflect.has(attributes, 'longitude')) {
                     let longitude = attributes["longitude"]
@@ -51,18 +76,21 @@ customElements.whenDefined("hui-view").then(() => {
                     if (attributes.map == 'gaode') {
                         point = MapGPS.gcj02_To_Bd09(latitude, longitude)
                     } else {
-                        point = MapGPS.gps84_To_Gcj02(latitude, longitude)
-                        point = MapGPS.gcj02_To_Bd09(point.lat, point.lon)
+                        point = MapGPS.gps84_To_Bd09(latitude, longitude)
                     }
                     longitude = point.lon
                     latitude = point.lat
                     const address = `${longitude},${latitude}`
 
+                    this.$('#name').textContent = attributes.friendly_name
                     const card = this.$('ha-card')
                     card.style.height = card.offsetWidth + 'px'
 
-                    const link = `url("http://api.map.baidu.com/staticimage/v2?ak=${ak}&zoom=${this.zoom}&width=1000&height=1000&center=${address}&markers=${address}")`
-                    if (link != card.backgroundImage) card.backgroundImage = link
+                    const src = `https://api.map.baidu.com/staticimage/v2?ak=${ak}&zoom=${this.zoom}&width=800&height=800&center=${address}&markers=${address}`
+                    const link = `url("${src}")`
+                    if (link != card.style.backgroundImage) {
+                        card.style.backgroundImage = link
+                    }
                 }
             }
         }
@@ -74,6 +102,10 @@ customElements.whenDefined("hui-view").then(() => {
             <div id="group">
                 <button id="plus">+</button>
                 <button id="subtract">-</button>
+            </div>
+            <div id="footer">
+                <button id="location">•</button>
+                <div id="name">实体名称</div>
             </div>
             `
             shadow.appendChild(map)
@@ -88,7 +120,7 @@ customElements.whenDefined("hui-view").then(() => {
                 top: 10px;
                 left: 10px;
             }
-            #group button{
+            #group button, #location{
                 display: block;
                 width: 30px;
                 height: 30px;
@@ -100,6 +132,21 @@ customElements.whenDefined("hui-view").then(() => {
             }
             #group #plus{
                 border-bottom: none;
+            }
+            #footer{                
+                position: absolute;
+                bottom: 10px;
+                left: 10px;
+            }
+            #location{                
+                border-radius: 50%;
+            }
+            #name{
+                color: white;
+                background: rgba(0,0,0,.4);
+                padding: 5px 10px;
+                border-radius: 10px;
+                margin-top: 10px;              
             }
             `
             shadow.appendChild(style);
@@ -124,16 +171,19 @@ customElements.whenDefined("hui-view").then(() => {
                 this.updated()
             }
 
+            this.$('#name').onclick = () => {
+                this.showMoreInfo(this.config.entity_id)
+            }
+
+            this.$('#location').onclick = () => {
+                center()
+            }
+
             // 移动画布
             let mouse = null
-            let mapWidth = card.offsetWidth
 
-            let backgroundPositionX = (mapWidth - 1000) / 2
-            let backgroundPositionY = (mapWidth - 1000) / 2
-            // map.style.width = mapWidth + 'px'
-            map.style.height = mapWidth + 'px'
-            map.style.backgroundPositionX = backgroundPositionX + 'px'
-            map.style.backgroundPositionY = backgroundPositionY + 'px'
+            let backgroundPositionX = undefined
+            let backgroundPositionY = undefined
 
             map.ontouchstart = function () {
                 const event = arguments[arguments.length - 1]
@@ -172,6 +222,17 @@ customElements.whenDefined("hui-view").then(() => {
                     mouse = null
                 }
             }
+
+            function center() {
+                let mapWidth = map.offsetWidth
+                backgroundPositionX = (mapWidth - 800) / 2
+                backgroundPositionY = (mapWidth - 800) / 2
+                map.style.height = mapWidth + 'px'
+                map.style.backgroundPositionX = backgroundPositionX + 'px'
+                map.style.backgroundPositionY = backgroundPositionY + 'px'
+            }
+
+            setTimeout(center, 0)
         }
 
     })
